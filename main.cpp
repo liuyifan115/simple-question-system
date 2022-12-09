c9ccc8bb31f506d96749ae92b45bc05da0b2915e
seele <liuyifan115@hotmail.com>
2022-12-09 16:14:13 +0800

    dev

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include "iostream"
+#include "ctime"
+#include "vector"
 
 using namespace std;
 
@@ -9,21 +11,25 @@ int query();
 int sort();
 int del();
 
-typedef struct score{
+typedef struct{
     int id;
-    struct score *next;
+    double score;
 }SCORE;
 
-typedef struct student{
-    char name[20];
-    int num;
-    SCORE score;
-    struct student *next;
+typedef struct{
+    string name;
+    string id;
+    vector<SCORE>score;
 }STUDENT;
 
-int main(int argc, char *argv[]){
-    menu();
-    return 0;
+vector<STUDENT> student = {{"null","null",{{0,0}}}};
+
+int main(){
+
+    while (true){
+        system("cls");
+        menu();
+    }
 }
 
 void menu(){
@@ -36,26 +42,32 @@ void menu(){
              << "\t\t\t\t5.成绩删除" << endl
              << "\t\t\t\t0.退出" << endl
              << "请选择：";
-        switch(getchar() - '0')
+        int choose = -1;
+        cin >> choose;
+        switch(choose)
         {
-            getchar();
             case 0: {
                 exit(1);
             }
             case 1:{
                 practise();
+                break;
             }
             case 2:{
                 exam();
+                break;
             }
             case 3:{
                 query();
+                break;
             }
             case 4:{
                 sort();
+                break;
             }
             case 5:{
                 del();
+                break;
             }
             default:{
                 system("cls");
@@ -67,17 +79,209 @@ void menu(){
 }
 
 int practise(){
-
+    int count = 0;
+    int ac = 0, wa = 0;
+    while (true){
+        system("cls");
+        srand(time(nullptr));
+        int num1 = rand()%101, num2 = rand()%101, operate = rand()%4+1;
+        if (operate == 4 and num1%num2 != 0){
+            continue;
+        }
+        int ans = -1;
+        int input = 0;
+        for (int i = 0; i < 3; ++i) {
+            cout << "当前第" << count+1 << "题\t\t正确率:" << 100*(double)ac/(double)count << "%\t\t输入114514退出练习" << endl << "请回答：" << endl;
+            switch (operate) {
+                case 1:{
+                    ans = num1 + num2;
+                    cout << num1 << "+" << num2 << "=";
+                    break;
+                }
+                case 2:{
+                    ans = num1 - num2;
+                    cout << num1 << "-" << num2 << "=";
+                    break;
+                }
+                case 3:{
+                    ans = num1 * num2;
+                    cout << num1 << "*" << num2 << "=";
+                    break;
+                }
+                case 4:{
+                    ans = num1 / num2;
+                    cout << num1 << "/" << num2 << "=";
+                    break;
+                }
+            }
+            cin >> input;
+            if (input == 114514){
+                break;
+            }
+            else if(input != ans and i != 2){
+                system("cls");
+                cout << "回答错误，您还有" << 2-i << "次机会，请重新回答！" << endl;
+                continue;
+            }
+            else if(input == ans){
+                break;
+            }
+            else{
+                cout << "回答错误，正确答案为："<< ans << "，按任意键进入下一题！" << endl;
+                system("pause");
+                break;
+            }
+        }
+        if (input == ans){
+            ac++;
+            count++;
+            cout << "回答正确，按任意键进入下一题！" << endl;
+            system("pause");
+            continue;
+        }
+        else if(input == 114514){
+            cout << "您本次作答了" << count << "道题目,\t正答数：" << ac << "\t错答数：" << wa << "\t正确率：" << 100*(double)ac/(double)count << "%" << endl;
+            system("pause");
+            break;
+        }
+        else{
+            wa++;
+            count++;
+            continue;
+        }
+    }
     return 0;
 }
 
-int exam(){
 
+int exam(){
+    string name,id;
+    int questionNumber = 0,count = 0,ac = 0;
+    typedef struct {
+        int num1;
+        int num2;
+        char operate;
+        int ans;
+        int input;
+    }QUESTION;
+    cout << "请依次输入你的姓名和准考证号" << endl;
+    cin >> name;
+    cin >> id;
+    cout << "请输入出题数量：";
+    cin >> questionNumber;
+    QUESTION question[questionNumber] = {};
+    for (int i = 0; i < questionNumber; ++i) {
+        system("cls");
+        srand(time(nullptr));
+        int num1 = 0, num2 = 0, operate = 0;
+        while(true){
+            num1 = rand()%101;
+            num2 = rand()%101;
+            operate = rand()%4+1;
+            if (operate == 4 and num1%num2 != 0){
+                continue;
+            }
+            else {
+                break;
+            }
+        }
+        question[i].num1 = num1;
+        question[i].num2 = num2;
+        int ans = -1;
+        int input = 0;
+        cout << "当前第" << count+1 << "题" << endl << "请回答：" << endl;
+        switch (operate) {
+            case 1:{
+                ans = num1 + num2;
+                question[i].operate = '+';
+                cout << num1 << "+" << num2 << "=";
+                break;
+            }
+            case 2:{
+                ans = num1 - num2;
+                question[i].operate = '-';
+                cout << num1 << "-" << num2 << "=";
+                break;
+            }
+            case 3:{
+                ans = num1 * num2;
+                question[i].operate = '*';
+                cout << num1 << "*" << num2 << "=";
+                break;
+            }
+            case 4:{
+                ans = num1 / num2;
+                question[i].operate = '/';
+                cout << num1 << "/" << num2 << "=";
+                break;
+            }
+        }
+        question[i].ans = ans;
+        cin >> input;
+        question[i].input = input;
+        if (input == ans){
+            ac++;
+            count++;
+            continue;
+        }
+        else{
+            count++;
+            continue;
+        }
+    }
+    system("cls");
+    cout << "考试结束，本次考试结果如下" << endl;
+    cout << "本次考试共" << questionNumber << "道题目，正答数：" << ac << "总分：" << 100*(double)ac/(double)count << endl
+         << "答题情况如下：" << endl << endl;
+    for (int i = 0; i < questionNumber; ++i) {
+        cout << "第" << i+1 << "题：";
+        if (question[i].ans == question[i].input){
+            cout << "√" << endl;
+        }
+        else {
+            cout << "X" << endl;
+        }
+        cout << question[i].num1 << question[i].operate << question[i].num2 << endl
+             << "正确答案" << question[i].ans << endl
+             << "你的答案" << question[i].input <<endl << endl << endl;
+    }
+    bool found = false;
+    for (auto iter = student.begin(); iter != student.end(); iter++) {
+        if (iter->id != id){
+            continue;
+        }
+        else{
+            int num = iter->score.size();
+            iter->score.push_back({num+1,100*(double)ac/(double)count});
+            found =! found;
+            break;
+        }
+    }
+    if (!found){
+        student.push_back({name,id,{{1,100*(double)ac/(double)count}}});
+    }
+    system("pause");
+    system("cls");
     return 0;
 }
 
 int query(){
-
+    string name,id;
+    system("cls");
+    cout << "请依次输入你的姓名和准考证号" << endl;
+    cin >> name;
+    cin >> id;
+    cout << endl;
+    for (auto iter1 = student.begin(); iter1 != student.end(); iter1++) {
+        if (iter1->id == id and iter1->name == name){
+            cout << "考试次数" << "\t考试成绩"<< endl;
+            for (auto iter2 = iter1->score.begin(); iter2 != iter1->score.end(); iter2++) {
+                cout << iter2->id << "\t\t" << iter2->score << endl << endl;
+            }
+        }
+    }
+    system("pause");
+    system("cls");
     return 0;
 }
 
